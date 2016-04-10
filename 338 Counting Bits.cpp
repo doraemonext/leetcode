e8f33c1efadfed59f4fb6109667c5270d4ce28bf
doraemonext <doraemonext@gmail.com>
2016-04-10 13:12:30 +0800

    Fix: fix 338

--- 338 Counting Bits.cpp
+++ 338 Counting Bits.cpp
@@ -3,24 +3,22 @@ public:
     vector<int> countBits(int num) {
         vector<int> binary;
         int step = 0;
-        binary.push_back(0);
-        for (; (1 << step) <= num; step++) {
+        for (; (1 << step) <= num; step++)
             binary.push_back(1 << step);
-        }
         binary.push_back(1 << step);
 
         vector<int> cnt;
-        int position = 0;
+        int position = 1;
         int max_binary = 0;
         cnt.push_back(0);
         if (num == 0) return cnt;
 
         for (int current = 1; current <= num; current++) {
-            while (current >= binary[position]) {
-                max_binary = binary[position];
+            while (current >= binary[position-1]) {
+                max_binary = binary[position-1];
                 position++;
             }
-            cnt.push_back(cnt[current - max_binary] + 1);
+            cnt.push_back(cnt[current-max_binary] + 1);
         }
 
         return cnt;

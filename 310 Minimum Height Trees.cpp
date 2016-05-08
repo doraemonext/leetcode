a567294d24f747d39eeb20e967c0d9a721ff02d2
doraemonext <doraemonext@gmail.com>
2016-05-08 19:22:32 +0800

    fix 310

--- 310 Minimum Height Trees.cpp
+++ 310 Minimum Height Trees.cpp
@@ -5,17 +5,14 @@ public:
         vector<int> answer;
         vector<pair<int, int>> leave_edges;
         vector<bool> empty(n, false);
+        int empty_count = 0;
 
         for (int i = 0; i < (int)edges.size(); i++) {
             graph[edges[i].first].push_back(edges[i].second);
             graph[edges[i].second].push_back(edges[i].first);
         }
 
-        while (true) {
-            answer.clear();
-            for (int i = 0; i < (int)leave_edges.size(); i++) {
-                answer.push_back(leave_edges[i].first);
-            }
+        while (empty_count <= n - 2) {
             leave_edges.clear();
 
             for (int i = 0; i < n; i++) {
@@ -34,21 +31,20 @@ public:
 
                 graph[from].clear();
                 graph[to].remove(from);
+                empty_count++;
                 empty[from] = true;
             }
         }
 
-        int empty_count = 0;
-        for (int i = 0; i < (int)empty.size(); i++) {
-            empty_count += (empty[i]) ? 1 : 0;
-        }
-        if (empty_count != n) {
-            answer.clear();
+        if (empty_count == n - 1) {
             for (int i = 0; i < n; i++) {
                 if (!empty[i]) {
                     answer.push_back(i);
-                    break;
                 }
+            }
+        } else {
+            for (int i = 0; i < (int)leave_edges.size(); i++) {
+                answer.push_back(leave_edges[i].first);
             }
         }
 
